3991cad37cbede80787a2dc0064ab82cdc91fb49
Adam Johnson <adamwjo@gmail.com>
2018-07-09 14:17:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var theBeats = []
+  for (let i = 0; i < 4; i++){
+    theBeats.push(`${musicians[i]} plays ${instruments[i]}`)
+  } return theBeats
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
+function johnLennonFacts(facts){
+  while (facts.length > 0){
+    facts.unshift('!!!')
+  } return facts
+}
